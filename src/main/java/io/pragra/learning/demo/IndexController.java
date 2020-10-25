feaf8cd7a84cb74ca79bddea07bbee2ffc08d703
Singh,Atin <atin@pragra.co>
2020-10-25 14:06:43 -0400

    Messed Code

--- src/main/java/io/pragra/learning/demo/IndexController.java
+++ src/main/java/io/pragra/learning/demo/IndexController.java
@@ -6,63 +6,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String getHome() {
-        String param = null;
-        int a  = 0;
-        int b = 0;
-        int sum = 0;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        sum = a+b;
-        if(param ==null || param =="") {
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-            System.out.println("Somthign is not right");
-        }
 
         return "index";
     }
